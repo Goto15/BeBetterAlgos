66443cafc6adefbc150da7348ecdce314aad9704
GoTo15 <github email address>
2020-06-22 13:46:11 -0500

    Range Sum of BST in C#

---
+++ RangeSumofBST.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int RangeSumBST(TreeNode root, int L, int R) {
+        if(root == null)
+        {
+            return 0;
+        }
+        
+        if(L <= root.val && root.val <= R)
+        {
+            return root.val + RangeSumBST(root.left, L, R)  + RangeSumBST(root.right, L, R);
+        } else 
+        {
+            return 0 + RangeSumBST(root.left, L, R)  + RangeSumBST(root.right, L, R);
+        }
+    }
+}
+
+/* Prompt 
+Given the root node of a binary search tree, return the sum of 
+values of all nodes with value between L and R (inclusive).
+
+The binary search tree is guaranteed to have unique values.
+*/
